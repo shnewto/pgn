f0268d5c6c0d17a4b9d59dd7cd8f26828d333d49
Carlos Felix <cj.felixrodrigues@gmail.com>
2023-11-10 23:59:14 +0000

    classic games parsed in tests (#15)
    Co-authored-by: carlos_felix <carlos@fetch.ly>

--- tests/ClassicPgnsTests.elm
+++ tests/ClassicPgnsTests.elm
@@ -1,4 +1,9 @@
-module ClassicPgnsTests exposing (testParseMovesPolgarVsKasparov, testParseTagPairPolgarVsKasparov)
+module ClassicPgnsTests exposing ( testParseMovesPolgarVsKasparov
+                                 , testParseTagPairPolgarVsKasparov
+                                 , testParseMovesSpasskyVsFischer
+                                 , testParseTagPairSpasskyVsFischer
+                                 , testParseMovesKramnikVsTopalov
+                                 , testParseTagPairKramnikVsTopalov )
 
 import Expect
 import Pgn
@@ -31,4 +36,58 @@ testParseMovesPolgarVsKasparov =
                 Err err ->
                     err
                         |> Pgn.parseErrorToString polgarVsKasparovPgn
+                        |> Expect.fail
+
+testParseMovesSpasskyVsFischer : Test
+testParseMovesSpasskyVsFischer =
+    test "Test that an example Spassky vs FIscher PGN parses a game's tag pair and produces an expected result" <|
+        \_ ->
+            case Pgn.parse spasskyVsFischerPgn of
+                Ok pgn ->
+                    Expect.equal pgn.tagPairs spasskyVsFischerPgnTagPairParsed
+
+                Err err ->
+                    err
+                        |> Pgn.parseErrorToString spasskyVsFischerPgn
+                        |> Expect.fail
+
+
+testParseTagPairSpasskyVsFischer : Test
+testParseTagPairSpasskyVsFischer =
+    test "Test that an example Spassky vs FIscher PGN parses a game's moves and produces an expected result" <|
+        \_ ->
+            case Pgn.parse spasskyVsFischerPgn of
+                Ok pgn ->
+                    Expect.equal pgn.moves spasskyVsFischerPgnMovesParsed
+
+                Err err ->
+                    err
+                        |> Pgn.parseErrorToString spasskyVsFischerPgn
+                        |> Expect.fail
+
+testParseMovesKramnikVsTopalov : Test
+testParseMovesKramnikVsTopalov =
+    test "Test that an example Kramnik vs Topalov PGN parses a game's tag pair and produces an expected result" <|
+        \_ ->
+            case Pgn.parse kramnikVsTopalovPgn of
+                Ok pgn ->
+                    Expect.equal pgn.tagPairs kramnikVsTopalovPgnTagPairParsed
+
+                Err err ->
+                    err
+                        |> Pgn.parseErrorToString kramnikVsTopalovPgn
+                        |> Expect.fail
+
+
+testParseTagPairKramnikVsTopalov : Test
+testParseTagPairKramnikVsTopalov =
+    test "Test that an example Kramnik vs Topalov PGN parses a game's moves and produces an expected result" <|
+        \_ ->
+            case Pgn.parse kramnikVsTopalovPgn of
+                Ok pgn ->
+                    Expect.equal pgn.moves kramnikVsTopalovPgnMovesParsed
+
+                Err err ->
+                    err
+                        |> Pgn.parseErrorToString kramnikVsTopalovPgn
                         |> Expect.fail
